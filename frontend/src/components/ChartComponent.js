8241cf32d3a258a783bef7db05d653b8556fb0fa
Tim Hillmann <thillman@charlotte.edu>
2025-05-05 03:53:56 +0000

    Fix pie chart connectors: true slice midpoints, fixed horizontal segment, vertical staggering

--- frontend/src/components/ChartComponent.js
+++ frontend/src/components/ChartComponent.js
@@ -1,49 +1,74 @@
 import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
 
-export default function ChartComponent({ data, labelKey, valueKey }) {
+export default function ChartComponent({ data, labelKey, valueKey, ariaLabel }) {
   const ref = useRef();
 
   useEffect(() => {
     if (!data.length) return;
+
+    // Clear out any existing chart
     const svg = d3.select(ref.current);
     svg.selectAll('*').remove();
-    const width = 400, height = 300;
-    svg.attr('viewBox', `0 0 ${width} ${height}`);
 
+    // Dimensions & margins
+    const fullWidth  = 480;
+    const fullHeight = 320;
+    const margin = { top: 20, right: 20, bottom: 80, left: 50 };
+    const width  = fullWidth  - margin.left - margin.right;
+    const height = fullHeight - margin.top  - margin.bottom;
+
+    svg
+      .attr('viewBox', `0 0 ${fullWidth} ${fullHeight}`)
+      .attr('role', 'img')
+      .attr('aria-label', ariaLabel);
+
+    // Scales
     const x = d3.scaleBand()
       .domain(data.map(d => d[labelKey]))
-      .range([40, width-20])
+      .range([0, width])
       .padding(0.1);
 
     const y = d3.scaleLinear()
       .domain([0, d3.max(data, d => d[valueKey])])
       .nice()
-      .range([height-30, 20]);
+      .range([height, 0]);
 
-    svg.append('g')
-      .attr('transform', `translate(0,${height-30})`)
+    // Color scale
+    const color = d3.scaleOrdinal(d3.schemeTableau10)
+      .domain(data.map(d => d[labelKey]));
+
+    // Chart group
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    // X axis
+    g.append('g')
+      .attr('transform', `translate(0,${height})`)
       .call(d3.axisBottom(x))
       .selectAll('text')
-      .attr('dy','0.35em')
-      .attr('x',-10)
-      .attr('y',10)
-      .attr('transform','rotate(-45)');
+        .attr('transform', 'rotate(-45)')
+        .attr('text-anchor', 'end')
+        .attr('dx', '-0.5em')
+        .attr('dy', '0.25em');
 
-    svg.append('g')
-      .attr('transform', `translate(40,0)`)
+    // Y axis
+    g.append('g')
       .call(d3.axisLeft(y));
 
-    svg.selectAll('.bar')
+    // Bars
+    g.selectAll('.bar')
       .data(data)
       .join('rect')
-      .attr('class','bar')
-      .attr('x', d => x(d[labelKey]))
-      .attr('y', d => y(d[valueKey]))
-      .attr('width', x.bandwidth())
-      .attr('height', d => height-30 - y(d[valueKey]));
+        .attr('class', 'bar')
+        .attr('x', d => x(d[labelKey]))
+        .attr('y', d => y(d[valueKey]))
+        .attr('width', x.bandwidth())
+        .attr('height', d => height - y(d[valueKey]))
+        .attr('fill', d => color(d[labelKey]));
 
-  }, [data]);
+  }, [data, labelKey, valueKey, ariaLabel]);
 
-  return <svg ref={ref} role="img" aria-label="Bar chart"></svg>;
+  return <svg ref={ref} />;
 }
+
